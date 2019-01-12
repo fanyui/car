39427418f7b80b9ece002855e3c9ab1f0ad31642
Harisu Fanyui <fanyuiharisu@gmail.com>
2019-01-12 21:47:27 +0100

    Fix display text

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,7 +14,7 @@
 			  // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
 			  // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
 			  var modal = $(this)
-			  modal.find('.modal-title').text('New message to ' + recipient)
+			  modal.find('.modal-title').text('Car Details Assign Worker')
 			  modal.find('.modal-body #recipient-name').val(recipient)
 			  modal.find('.modal-body #input-mark').val(car_mark)
 			  modal.find('.modal-body #input-owner').val(car_owner)
@@ -90,7 +90,7 @@
 		                        	<tr>
 		                                <td>{{$unassignedCar->mark}}</td>
 		                                <td>{{$unassignedCar->plate_number}}</td>
-		                                <td><button id="btn-new" type="button" class="btn btn-primary" data-toggle="modal" data-target="#newModal" data-whatever="@getbootstrap" data-mark="{{$unassignedCar->mark}}"  data-owner="{{$unassignedCar->owner}}" data-plate="{{$unassignedCar->plate_number}}" data-id="{{$unassignedCar->id}}">View</button>
+		                                <td><button id="btn-new" type="button" class="btn btn-primary" data-toggle="modal" data-target="#newModal" data-mark="{{$unassignedCar->mark}}"  data-owner="{{$unassignedCar->owner}}" data-plate="{{$unassignedCar->plate_number}}" data-id="{{$unassignedCar->id}}">View</button>
 		                                </td>
 		                            </tr>
 		                        @endforeach
@@ -174,7 +174,7 @@
 			  <div class="modal-dialog" role="document">
 			    <div class="modal-content">
 			      <div class="modal-header">
-			        <h5 class="modal-title" id="exampleModalLabel">New message</h5>
+			        <h5 class="modal-title" id="exampleModalLabel">Car Details</h5>
 			        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
 			          <span aria-hidden="true">&times;</span>
 			        </button>
@@ -236,7 +236,7 @@
 			  <div class="modal-dialog" role="document">
 			    <div class="modal-content">
 			      <div class="modal-header">
-			        <h5 class="modal-title" id="exampleModalLabel">New message</h5>
+			        <h5 class="modal-title" id="exampleModalLabel">Car Detauls</h5>
 			        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
 			          <span aria-hidden="true">&times;</span>
 			        </button>
@@ -301,7 +301,7 @@
 			  <div class="modal-dialog" role="document">
 			    <div class="modal-content">
 			      <div class="modal-header">
-			        <h5 class="modal-title" id="exampleModalLabel">New message</h5>
+			        <h5 class="modal-title" id="exampleModalLabel">Car Details</h5>
 			        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
 			          <span aria-hidden="true">&times;</span>
 			        </button>
